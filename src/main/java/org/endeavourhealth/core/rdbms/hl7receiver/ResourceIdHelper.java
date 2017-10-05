a60fb2e40d8854ead0bc4bf261b8d6135c382499
Richard Collier <ergosoftuk@gmail.com>
2017-10-05 18:34:39 +0100

    Switched ResourceIdHelper.getResourceId() from getSingleResult() to getResultList()

--- src/main/java/org/endeavourhealth/core/rdbms/hl7receiver/ResourceIdHelper.java
+++ src/main/java/org/endeavourhealth/core/rdbms/hl7receiver/ResourceIdHelper.java
@@ -1,11 +1,9 @@
 package org.endeavourhealth.core.rdbms.hl7receiver;
 
-import org.endeavourhealth.core.rdbms.hl7receiver.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import java.util.*;
 
@@ -15,6 +13,9 @@ public class ResourceIdHelper {
 
     public static ResourceId getResourceId(String scope, String resource, String uniqueId) throws Exception {
         EntityManager entityManager = HL7ReceiverConnection.getEntityManager();
+
+        if (!entityManager.isOpen())
+            throw new IllegalStateException("No connection to HL7 DB");
 
         String sql = "select c"
                 + " from"
@@ -26,15 +27,15 @@ public class ResourceIdHelper {
         Query query = entityManager.createQuery(sql, ResourceId.class)
                 .setParameter("scopeId", scope).setParameter("resourceType", resource).setParameter("uniqueId", uniqueId);
 
-        try {
-            ResourceId ret = (ResourceId)query.getSingleResult();
-            LOG.trace("Read recourceId:" + ret.getUniqueId() + "==>" + ret.getResourceId());
-            return ret;
-
-        } catch (NoResultException ex) {
+        List results = query.getResultList();
+        if (results.isEmpty())
             return null;
 
-        }
+        ResourceId ret = (ResourceId)results.get(0);
+
+        LOG.trace("Read recourceId:" + ret.getUniqueId() + "==>" + ret.getResourceId());
+        return ret;
+
     }
 
     public static void saveResourceId(ResourceId resourceId)  throws Exception {
