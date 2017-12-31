e6e4d34f576464b76cdfafe05e149d46b4cbbcf9
Drew Littler <drewlittler@hotmail.com>
2017-12-31 16:31:25 +0000

    Taken out code caching of callable statements

--- src/main/java/org/endeavourhealth/core/database/rdbms/CallableStatementCache.java
+++ src/main/java/org/endeavourhealth/core/database/rdbms/CallableStatementCache.java
@@ -1,26 +1,47 @@
 package org.endeavourhealth.core.database.rdbms;
 
 import org.hibernate.internal.SessionImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.sql.SQLException;
 
 public class CallableStatementCache {
+    private static final Logger LOG = LoggerFactory.getLogger(CallableStatementCache.class);
 
-    private Map<EntityManager, ConcurrentLinkedDeque<CallableStatement>> cache = new ConcurrentHashMap<>();
+    //private Map<EntityManager, ConcurrentLinkedDeque<CallableStatement>> cache = new ConcurrentHashMap<>();
     private String sql = null;
 
     public CallableStatementCache(String sql) {
         this.sql = sql;
     }
 
+    /**
+     * NOTE: all caching has been taken out of this class, as the MySQL Java driver supports caching
+     * which is now enabled in the persistence.xml file
+     */
     public CallableStatement getCallableStatement(EntityManager entityManager) throws Exception {
+
+        SessionImpl session = (SessionImpl) entityManager.getDelegate();
+        Connection connection = session.connection();
+        return connection.prepareCall(sql);
+    }
+
+    public void returnCallableStatement(EntityManager entityManager, CallableStatement callableStatement) {
+        //just close the statement
+        try {
+            callableStatement.close();
+        } catch (SQLException ex) {
+            LOG.error("Error closing prepared statement " + sql, ex);
+        }
+    }
+
+    /*public CallableStatement getCallableStatement(EntityManager entityManager) throws Exception {
         ConcurrentLinkedDeque<CallableStatement> queue = findOrCreateQueue(entityManager);
+        LOG.info("Queue for " + sql + " now " + queue.size());
 
         CallableStatement ret = null;
 
@@ -46,6 +67,7 @@ public class CallableStatementCache {
 
         ConcurrentLinkedDeque<CallableStatement> queue = findOrCreateQueue(entityManager);
         queue.push(callableStatement);
+        LOG.info("Queue for " + sql + " now " + queue.size());
     }
 
     private ConcurrentLinkedDeque<CallableStatement> findOrCreateQueue(EntityManager entityManager) {
@@ -61,5 +83,5 @@ public class CallableStatementCache {
             }
         }
         return queue;
-    }
+    }*/
 }
