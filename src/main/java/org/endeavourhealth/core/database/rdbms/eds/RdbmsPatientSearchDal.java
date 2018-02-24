17046926aaea52d98c96f86000e3a5ccce6076af
Drew Littler <drewlittler@hotmail.com>
2018-02-24 11:27:45 +0000

    Updated to refactored FHIR URI classes in FHIR repo

--- src/main/java/org/endeavourhealth/core/database/rdbms/eds/RdbmsPatientSearchDal.java
+++ src/main/java/org/endeavourhealth/core/database/rdbms/eds/RdbmsPatientSearchDal.java
@@ -323,7 +323,7 @@ public class RdbmsPatientSearchDal implements PatientSearchDalI {
         if (fhirPatient.hasIdentifier()) {
             for (Identifier fhirIdentifier : fhirPatient.getIdentifier()) {
 
-                if (!fhirIdentifier.getSystem().equalsIgnoreCase(FhirUri.IDENTIFIER_SYSTEM_NHSNUMBER)) {
+                if (!fhirIdentifier.getSystem().equalsIgnoreCase(FhirIdentifierUri.IDENTIFIER_SYSTEM_NHSNUMBER)) {
                     String system = fhirIdentifier.getSystem();
                     String value = fhirIdentifier.getValue();
 
