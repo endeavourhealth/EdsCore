d5d205f877fff95a8438c2988bc248980cadf306
Drew Littler <drewlittler@hotmail.com>
2018-04-10 11:55:54 +0100

    Temporary fix to handle JSON with missing escapes

--- src/main/java/org/endeavourhealth/core/fhirStorage/FhirSerializationHelper.java
+++ src/main/java/org/endeavourhealth/core/fhirStorage/FhirSerializationHelper.java
@@ -18,6 +18,31 @@ public class FhirSerializationHelper {
         try {
             return PARSER_POOL.parse(resourceAsJsonString);
         } catch (Exception e) {
+
+            //nasty hack to get around JSON that was saved without escaping the \\ before double quotes that MySQL needs
+            //data is being fixed, but in the short-term this will handle the missing \\ chars
+            String msg = e.getMessage();
+            String prefix = "Unterminated object at line 1 column ";
+            int index = msg.indexOf(prefix);
+            if (index > -1) {
+                String suffix = msg.substring(index + prefix.length());
+                index = suffix.indexOf(" ");
+                String sub = suffix.substring(0, index);
+                int pos = Integer.parseInt(sub);
+
+                String jsonBefore = resourceAsJsonString.substring(0, pos - 3);
+                String jsonAfter = resourceAsJsonString.substring(pos - 2);
+
+                int nextPos = jsonAfter.indexOf("\"");
+                if (nextPos > -1) {
+                    String middle = jsonAfter.substring(0, nextPos);
+                    jsonAfter = jsonAfter.substring(nextPos+1);
+
+                    resourceAsJsonString = jsonBefore + "\\\"" + middle + "\\\"" + jsonAfter;
+                    return deserializeResource(resourceAsJsonString);
+                }
+            }
+
             throw new SerializationException(e);
         }
     }
