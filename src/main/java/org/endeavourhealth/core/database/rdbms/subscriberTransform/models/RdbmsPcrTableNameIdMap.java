2d07d0cd9387d04e4c6afc05cc57751f07ce733b
Drew Littler <drewlittler@hotmail.com>
2018-11-20 15:26:00 +0000

    Fix to new PCR hibernate entity

--- src/main/java/org/endeavourhealth/core/database/rdbms/subscriberTransform/models/RdbmsPcrTableNameIdMap.java
+++ src/main/java/org/endeavourhealth/core/database/rdbms/subscriberTransform/models/RdbmsPcrTableNameIdMap.java
@@ -5,17 +5,19 @@ import org.hibernate.annotations.GenerationTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "pcr_tables")
 
-public class RdbmsPcrTableNameIdMap  implements Serializable  {
+public class RdbmsPcrTableNameIdMap implements Serializable {
 
     private long id;
     private String tableName = null;
 
+    @Id
     @Generated(GenerationTime.INSERT)
     @Column(name = "id", insertable = false)
     public long getId() {
@@ -31,7 +33,8 @@ public class RdbmsPcrTableNameIdMap  implements Serializable  {
         return tableName;
     }
 
-    public void setTableName(String tableName) {this.tableName = tableName;
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
     }
 
 
