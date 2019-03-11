7f441358adac853617e50cb14f5f65ad89b4c06a
JamesBiswellEnd <james.biswell@endeavourhealth.org>
2019-03-11 17:29:43 +0000

    Adding English hospital consultants current DAL.

---
+++ src/main/java/org/endeavourhealth/core/database/rdbms/reference/RdbmsEhccDal.java
@@ -0,0 +1,13 @@
+package org.endeavourhealth.core.database.rdbms.reference;
+
+import org.endeavourhealth.core.database.dal.reference.models.EhccDalI;
+
+public class RdbmsEhccDal implements EhccDalI {
+
+    public String lookupCode(String opcs4Code) throws Exception {
+        return "";
+    }
+
+    public void updateEhccLookup(String[] ehccData) throws Exception {
+    }
+}
