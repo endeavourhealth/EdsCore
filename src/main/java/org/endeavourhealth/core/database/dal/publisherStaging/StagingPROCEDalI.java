bd24d2a0a0d23377915b36a25f724653ace61229
Drew Littler <drewlittler@hotmail.com>
2019-04-30 14:57:24 +0100

    Changed patient search tables to keep records for deleted patients

--- src/main/java/org/endeavourhealth/core/database/dal/publisherStaging/StagingPROCEDalI.java
+++ src/main/java/org/endeavourhealth/core/database/dal/publisherStaging/StagingPROCEDalI.java
@@ -1,14 +1,12 @@
 package org.endeavourhealth.core.database.dal.publisherStaging;
 
 import org.endeavourhealth.core.database.dal.publisherStaging.models.StagingPROCE;
-import org.hl7.fhir.instance.model.Enumerations;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface StagingPROCEDalI {
 
     boolean getRecordChecksumFiled(UUID serviceId, StagingPROCE stagingPROCE) throws Exception;
     void save(StagingPROCE stagingPROCE, UUID serviceId) throws Exception;
-    List<UUID> getSusResourceMappings(UUID serviceId, String sourceRowId, Enumerations.ResourceType resourceType) throws Exception;
+    //List<UUID> getSusResourceMappings(UUID serviceId, String sourceRowId, Enumerations.ResourceType resourceType) throws Exception;
 }
