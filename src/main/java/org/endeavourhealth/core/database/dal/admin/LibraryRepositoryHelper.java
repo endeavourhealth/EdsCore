72f2a0bea89335cacc07c0d3b8c687501ed88091
Drew Littler <drewlittler@hotmail.com>
2019-05-30 13:25:53 +0100

    Removed logging

--- src/main/java/org/endeavourhealth/core/database/dal/admin/LibraryRepositoryHelper.java
+++ src/main/java/org/endeavourhealth/core/database/dal/admin/LibraryRepositoryHelper.java
@@ -8,6 +8,8 @@ import org.endeavourhealth.core.database.dal.admin.models.DefinitionItemType;
 import org.endeavourhealth.core.database.dal.admin.models.Item;
 import org.endeavourhealth.core.xml.QueryDocument.*;
 import org.endeavourhealth.core.xml.QueryDocument.System;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class LibraryRepositoryHelper {
+	private static final Logger LOG = LoggerFactory.getLogger(LibraryRepositoryHelper.class);
 
 	//disable validation when READING XML (it's still done when writing)
 	//private static final String XSD = "QueryDocument.xsd";
@@ -41,11 +44,16 @@ public class LibraryRepositoryHelper {
 				items.add(item);
 			}
 		}
+		//LOG.trace("Found " + items.size() + " protocols to check for service " + serviceId + " and system " + systemId);
 
 		List<LibraryItem> ret = new ArrayList<>();
 
 		for (int i = 0; i < items.size(); i++) {
 			Item item = items.get(i);
+			//LOG.trace("Checking protocol " + item.getTitle());
+
+			boolean addItem = false;
+
 			String xml = item.getXmlContent();
 			LibraryItem libraryItem = XmlSerializer.deserializeFromString(LibraryItem.class, xml, null);
 			//LibraryItem libraryItem = XmlSerializer.deserializeFromString(LibraryItem.class, xml, XSD);
@@ -53,35 +61,52 @@ public class LibraryRepositoryHelper {
 			List<ServiceContract> serviceContracts = protocol.getServiceContract();
 			for (int s = 0; s < serviceContracts.size(); s++) {
 				ServiceContract serviceContract = serviceContracts.get(s);
+
+				//filter on service
 				String serviceContractServiceId = serviceContract.getService().getUuid();
-				String serviceContractSystemId = serviceContract.getSystem().getUuid();
-
-				if (serviceContractServiceId.equals(serviceId)
-						&& (Strings.isNullOrEmpty(systemId) || serviceContractSystemId.equals(systemId))) {
-
-					// Load full system details
-					String systemUuid = serviceContract.getSystem().getUuid();
-					ActiveItem activeSystemItem = repository.getActiveItemByItemId(UUID.fromString(systemUuid));
-					Item systemItem = repository.getItemByKey(activeSystemItem.getItemId(), activeSystemItem.getAuditId());
-					String systemLibraryItemXml = systemItem.getXmlContent();
-
-					LibraryItem systemLibraryItem = XmlSerializer.deserializeFromString(LibraryItem.class, systemLibraryItemXml, null);
-					//LibraryItem systemLibraryItem = XmlSerializer.deserializeFromString(LibraryItem.class, systemLibraryItemXml, XSD);
-					System system = systemLibraryItem.getSystem();
-					serviceContract.setSystem(system);
-
-					TechnicalInterface technicalInterface = system.getTechnicalInterface().stream()
-							.filter(ti -> ti.getUuid().equals(serviceContract.getTechnicalInterface().getUuid()))
-							.findFirst()
-							.orElse(null);
-
-					if (technicalInterface != null) {
-						serviceContract.setTechnicalInterface(technicalInterface);
-
-						ret.add(libraryItem);
-						break; //added so we don't add protocols multiple times if there are multiple matching service contracts
-					}
+				//LOG.trace("Check contract service " + serviceContractServiceId);
+				if (!serviceContractServiceId.equals(serviceId)) {
+					//LOG.trace("Skipping contract");
+					continue;
 				}
+
+				//filter on optional system
+				String serviceContractSystemId = serviceContract.getSystem().getUuid();
+				//LOG.trace("Check contract system " + serviceContractSystemId);
+				if (!Strings.isNullOrEmpty(systemId)
+						&& !serviceContractSystemId.equals(systemId)) {
+					//LOG.trace("Skipping contract");
+					continue;
+				}
+
+				// Load full system details
+				String systemUuid = serviceContract.getSystem().getUuid();
+				ActiveItem activeSystemItem = repository.getActiveItemByItemId(UUID.fromString(systemUuid));
+				Item systemItem = repository.getItemByKey(activeSystemItem.getItemId(), activeSystemItem.getAuditId());
+				String systemLibraryItemXml = systemItem.getXmlContent();
+
+				LibraryItem systemLibraryItem = XmlSerializer.deserializeFromString(LibraryItem.class, systemLibraryItemXml, null);
+				//LibraryItem systemLibraryItem = XmlSerializer.deserializeFromString(LibraryItem.class, systemLibraryItemXml, XSD);
+				System system = systemLibraryItem.getSystem();
+				serviceContract.setSystem(system);
+
+				TechnicalInterface technicalInterface = system.getTechnicalInterface().stream()
+						.filter(ti -> ti.getUuid().equals(serviceContract.getTechnicalInterface().getUuid()))
+						.findFirst()
+						.orElse(null);
+
+				if (technicalInterface != null) {
+					serviceContract.setTechnicalInterface(technicalInterface);
+				}
+
+				//LOG.trace("Will add protocol");
+				addItem = true;
+				//don't break out, so we continue and populate the technical interface
+				//details for any other service contracts for this service
+			}
+
+			if (addItem) {
+				ret.add(libraryItem);
 			}
 		}
 
