e4ba1e14e123d61a2dee24131e8d0e498a7ac98b
Drew Littler <drewlittler@hotmail.com>
2020-07-06 15:07:15 +0100

    Removed a load of unused database code

--- src/main/java/org/endeavourhealth/core/database/dal/subscriberTransform/VitruCareTransformDalI.java
+++
@@ -1,9 +0,0 @@
-package org.endeavourhealth.core.database.dal.subscriberTransform;
-
-import java.util.UUID;
-
-public interface VitruCareTransformDalI {
-
-    void saveVitruCareIdMapping(UUID edsPatientId, UUID serviceId, UUID systemId, String virtruCareId) throws Exception;
-    String getVitruCareId(UUID edsPatientId) throws Exception;
-}
